a2d0ff5496155eace90c502d20940f807720caa3
muhammad <muhammadraja0300@gmail.com>
2024-10-15 03:08:19 +0500

    add jsonreport in testrunner

--- src/test/java/StepDefinitions/TestRunner.java
+++ src/test/java/StepDefinitions/TestRunner.java
@@ -9,7 +9,7 @@ import org.junit.runner.RunWith;
     features= "src/test/java/Features/login.feature",
     glue={"StepDefinitions"},
     //tags= "@regression" ,
-    plugin = {"pretty" , "html:target/cucumber-reports/report.html" , "json:target/cucumber.json"}
+    plugin = {"pretty" , "html:target/cucumber-reports/report.html" , "json:target/cucumber.json" , "json:target/report.json"}
     
     
 )

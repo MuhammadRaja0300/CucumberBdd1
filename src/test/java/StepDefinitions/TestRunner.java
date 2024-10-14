c80fa770a9bca7847c65e59c72fd2b4e188093e5
muhammad <muhammadraja0300@gmail.com>
2024-10-15 01:26:48 +0500

    Initial commit

---
+++ src/test/java/StepDefinitions/TestRunner.java
@@ -0,0 +1,18 @@
+package StepDefinitions;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+    features= "src/test/java/Features/login.feature",
+    glue={"StepDefinitions"},
+    //tags= "@regression" ,
+    plugin = {"pretty" , "html:target/cucumber-reports/report.html" , "json:target/cucumber.json"}
+    
+    
+)
+public class TestRunner {
+}
+

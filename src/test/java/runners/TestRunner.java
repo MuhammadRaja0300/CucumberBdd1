9fb8f44d56e2cb86814310d6fb5ecc1fb2a59ffd
muhammad <muhammadraja0300@gmail.com>
2024-10-19 02:19:11 +0500

    update loginstep

--- src/test/java/runners/TestRunner.java
+++ src/test/java/runners/TestRunner.java
@@ -6,7 +6,7 @@ import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
-    features= "src/test/java/features",
+    features= "src/test/java/features/login.feature",
     glue={"stepDefinitions"},
     //tags= "@regression" ,
         plugin = {"pretty",
